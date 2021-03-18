bc637f50d469e0a850e99956f2ef39c5da67c0fc
GauthamBellamkonda <73273020+GauthamBellamkonda@users.noreply.github.com>
2021-03-18 16:06:55 +0530

    Add files via upload

---
+++ Assignment3/Codes/Assignment3_plot.py
@@ -0,0 +1,28 @@
+import numpy as np
+import scipy 
+import math
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+
+a = 1
+beta = 0.5
+mean1 = a*(1+beta)
+mean2 = -a*(1+beta)
+standard_dev = 1
+x = np.arange(-6,6, 0.001)
+y1 = norm(mean1, standard_dev)
+y2 = norm(mean2, standard_dev)
+x_values = [0.0, 0.0]
+y_values = [0.0, 0.5]
+plt.plot(x_values, y_values, color = 'black')
+plt.plot(x,y1.pdf(x))
+plt.plot(x,y2.pdf(x))
+plt.grid()
+
+plt.ylabel("Probability")
+plt.xlabel("u - axis")
+plt.fill_between(x,0,y1.pdf(x),where=y1.pdf(x) < y2.pdf(x),color='C0', alpha = 0.4, interpolate=True)
+plt.fill_between(x,0,y2.pdf(x),where=y2.pdf(x) < y1.pdf(x),color='C1', alpha = 0.4, interpolate=True)
+plt.legend(["Threshold of the detector","$\Pr(Y=u , X =+a)$","$\Pr(Y=u , X =-a)$", "BER$_{+a}$", "BER$_{-a}$"], prop={'size': 7})
+plt.title("Representing BER as area under a graph") 
+plt.show()
