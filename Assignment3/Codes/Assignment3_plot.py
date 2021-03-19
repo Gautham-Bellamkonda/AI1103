d0fb084c69204a2b24f0bd8c7db47eff5c07ef04
GauthamBellamkonda <73273020+GauthamBellamkonda@users.noreply.github.com>
2021-03-19 19:23:38 +0530

    Add files via upload

--- Assignment3/Codes/Assignment3_plot.py
+++ Assignment3/Codes/Assignment3_plot.py
@@ -12,17 +12,23 @@ standard_dev = 1
 x = np.arange(-6,6, 0.001)
 y1 = norm(mean1, standard_dev)
 y2 = norm(mean2, standard_dev)
+k = 0.0668072
 x_values = [0.0, 0.0]
-y_values = [0.0, 0.5]
-plt.plot(x_values, y_values, color = 'black')
-plt.plot(x,y1.pdf(x))
-plt.plot(x,y2.pdf(x))
+y_values = [k, 1-k]
+plt.plot(x_values, y_values,'black')
+plt.plot(x,y1.cdf(x))
+plt.plot(x,y2.cdf(x))
 plt.grid()
 
-plt.ylabel("$\Pr(Y=y)$")
+
+y_values = [0.0, k]
+plt.plot(x_values, y_values,'x-')
+
+y_values = [1-k, 1]
+plt.plot(x_values, y_values,'x-')
+
+plt.ylabel("$F_Y(y)$")
 plt.xlabel("y - axis")
-plt.fill_between(x,0,y1.pdf(x),where=y1.pdf(x) < y2.pdf(x),color='C0', alpha = 0.4, interpolate=True)
-plt.fill_between(x,0,y2.pdf(x),where=y2.pdf(x) < y1.pdf(x),color='C1', alpha = 0.4, interpolate=True)
-plt.legend(["Threshold of the detector","$\Pr(Y=y , X =+a)$","$\Pr(Y=y , X =-a)$", "BER$_{+a}$", "BER$_{-a}$"], prop={'size': 7})
-plt.title("Representing BER as area under a graph") 
+plt.legend(["Threshold of the detector","$F_Y(y , X =+a)$","$F_Y(y , X =-a)$", "BER$_{+a}$", "BER$_{-a}$"], prop={'size': 7})
+plt.title("Representing BER in CDF of $Y$") 
 plt.show()
