77a3a48c57c85627a3e57f0f165082dcab1f5444
GauthamBellamkonda <73273020+GauthamBellamkonda@users.noreply.github.com>
2021-04-02 11:03:49 +0530

    Added plots, codes in qn 8

--- probman/codes/twoD/scatter_plot.py
+++ probman/codes/twoD/scatter_plot.py
@@ -13,8 +13,8 @@ plt.plot( y1, y2, 'bx')
 n1, n2 = np.random.multivariate_normal(mean, cov, simlen).T
 y1 = n1
 y2 = A + n2
-plt.plot( y1, y2, 'bx')
-
+plt.plot(y1, y2, 'rx')
+plt.legend(['$\mathbf{x} = \mathbf{s}_0$','$\mathbf{x} = \mathbf{s}_1$'])
 plt.xlabel("$y_1$")
 plt.ylabel("$y_2$")
 
